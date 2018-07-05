ccef3749b0f87d3128f9d676bf1e8a6c7360b12d
Kevin Perez <cronox030@gmail.com>
2018-07-05 11:45:45 -0400

    add graphql

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,5 @@
 export default {
-  PORT: process.env.PORT || 3000,
-  DB_URL: 'mongodb://localhost/1#app-server-development'
-}
+  PORT: process.env.PORT || 4000,
+  DB_URL: 'mongodb://localhost/1#app-server-development',
+  GRAPHQL_PATH: '/graphql',
+};
--- src/index.js
+++ src/index.js
@@ -2,15 +2,40 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
-
-import constants from './config/constants';
+import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import { createServer } from 'http';
 import './config/db';
+import constants from './config/constants';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
 
 const app = express();
 
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
 app.use(bodyParser.json());
 
-app.listen(constants.PORT, err => {
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: constants.GRAPHQL_PATH,
+  }),
+);
+
+app.use(
+  constants.GRAPHQL_PATH,
+  graphqlExpress({
+    schema,
+  }),
+);
+
+const graphQLServer = createServer(app);
+
+graphQLServer.listen(constants.PORT, err => {
   if (err) {
     console.log(err);
   } else {
